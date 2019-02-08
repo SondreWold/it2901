ddf8a4a55533f7861d4003b28d5189e0f6637dd3
sondre <sondrewold1@gmail.com>
2019-02-08 11:07:13 +0100

    removed comments #7

--- server/app.js
+++ server/app.js
@@ -5,21 +5,24 @@ import logger from 'morgan';
 import bb from 'express-busboy';
 import SourceMapSupport from 'source-map-support';
 
-// define our app using express
 const app = express();
+
 // express-busboy to parse multipart/form-data
 bb.extend(app);
+
 // allow-cors
 app.use(function(req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
+
 // configure app
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 // set the port
 const port = process.env.PORT || 3001;
 
@@ -29,10 +32,10 @@ SourceMapSupport.install();
 app.get('/', (req,res) => {
   return res.end('Api working');
 })
-// catch 404
 app.use((req, res, next) => {
   res.status(404).send('<h2 align=center>Page Not Found!</h2>');
 });
+
 // start the server
 app.listen(port,() => {
   console.log(`App Server Listening at ${port}`);
