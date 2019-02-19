ee50348b2f3932290f9a919e0e7bee48d62f6f5f
sondre <sondrewold1@gmail.com>
2019-02-19 16:26:40 +0100

    Added action for fetching absent children #33

--- client/src/containers/ContentContainer.js
+++ client/src/containers/ContentContainer.js
@@ -3,16 +3,19 @@ import { connect } from "react-redux";
 import { getBases } from "../actions/contentActions/contentBaseActions";
 import { getEmployees } from "../actions/contentActions/contentEmployeeActions";
 import { getAbsentEmployees } from "../actions/contentActions/contentAbsenceEmployeeActions";
+import { getAbsentChildren } from "../actions/contentActions/contentAbsenceChildrenActions";
 
 class contentContainer extends React.Component {
   componentDidMount() {
     this.props.getBases();
     this.props.getEmployees();
     this.props.getAbsentEmployees();
+    this.props.getAbsentChildren();
   }
 
+  //Her skal komponentene som skal få data fra denne containeren ligge. Send ned den aktuelle dataen via props
   render() {
-    return <p>Yo, sjekk redux logg for å kontrollere at shit har kommet</p>;
+    return <p>Content Container //ToBeRemoved </p>;
   }
 }
 
@@ -20,14 +23,16 @@ const mapDispatchToProps = dispatch => {
   return {
     getBases: url => dispatch(getBases()),
     getEmployees: url => dispatch(getEmployees()),
-    getAbsentEmployees: url => dispatch(getAbsentEmployees())
+    getAbsentEmployees: url => dispatch(getAbsentEmployees()),
+    getAbsentChildren: url => dispatch(getAbsentChildren())
   };
 };
 
 const mapStateToProps = state => ({
   bases: state.contentBase.bases,
   employees: state.contentEmployee.employees,
-  absentEmployees: state.contentAbsentEmployees.absentEmployees
+  absentEmployees: state.contentAbsentEmployees.absentEmployees,
+  absentChildren: state.contentAbsentChildren.absentChildren
 });
 
 export default connect(
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import contentBase from "./contentReducers/contentBaseReducer";
 import contentEmployee from "./contentReducers/contentEmployeeReducer";
 import contentAbsentEmployees from "./contentReducers/contentAbsenceEmployeeReducer";
+import contentAbsentChildren from "./contentReducers/contentAbsenceChildrenReducer";
 
 export default combineReducers({
   //import in all reducers here
   contentBase,
   contentEmployee,
-  contentAbsentEmployees
+  contentAbsentEmployees,
+  contentAbsentChildren
 });
