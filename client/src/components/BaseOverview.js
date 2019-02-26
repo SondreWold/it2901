f98f6c9313dc48521c4b477b719c2cdff369dfb1
ulrikah <ulrikhalmoy@gmail.com>
2019-02-26 09:28:47 +0100

    receive date as props in BaseOverview #66

--- client/src/components/BaseOverview.js
+++ client/src/components/BaseOverview.js
@@ -9,17 +9,15 @@ class BaseOverview extends Component {
 	formatQuery = (moved_employees, bases, employees) => {
 		let data = { employees: {}, columns: {}, columnOrder: []};
 		
-		employees.map( (e) => {
+		employees.forEach( (e) => {
 			const id = "employee-" + e.id;
 			data.employees[id] = { id: id, content: e.first_name};
 		});
-		bases.map( (b) => {
+		bases.forEach( (b) => {
 			const id = "column-" + b.id; 
 			data.columnOrder.push(id);
 			data.columns[id] = { id: id, title: b.name, employeeIds: []}
 		});
-
-
 
   	for (let i = 0; i < moved_employees.length; i++) {
   		for (let j = 0; j < bases.length; j++){
@@ -36,7 +34,7 @@ class BaseOverview extends Component {
 		const formatted_data = this.formatQuery(this.props.moved_employees, this.props.bases, this.props.employees)
   	return(
   		<div>
-  			<BaseCardHolder data={formatted_data}/>
+  			<BaseCardHolder date={this.props.date} data={formatted_data}/>
 			</div>
   	);
   }
@@ -44,43 +42,3 @@ class BaseOverview extends Component {
 }
 
 export default BaseOverview;
-
-
-/*
-const initialData = {
-
-
-    employees: {
-    	"employee-1": { id: "employee-1", content: "Aksel Andersen" },
-    	"employee-2": { id: "employee-2", content: "Beate Brun" },
-    	"employee-3": { id: "employee-3", content: "Charlotte Chill" },
-    	"employee-4": { id: "employee-4", content: "Dorthe Dor" },
-    	"employee-5": { id: "employee-5", content: "Eivind Eiriksen" },
-    	"employee-6": { id: "employee-6", content: "Fredrik Fisk" },
-    	"employee-7": { id: "employee-7", content: "Geir Gudmundsen" },
-    	"employee-8": { id: "employee-8", content: "Henriette Hågård" },
-    },
-
-      columns : {
-
-    	"column-1": {
-    		id: "column-1",
-    		title: "",
-    		employeeIds: ["employee-1", "employee-2","employee-3", "employee-4"]
-    	},
-    	"column-2": {
-    		id: "column-2",
-    		title: "In progress",
-    		employeeIds: ["employee-5", "employee-6"]
-    	},
-    	"column-3": {
-    		id: "column-3",
-    		title: "Done",
-    		employeeIds: ["employee-7", "employee-8"]
-    	}
-    },
-
-    // facilitate reordering
-    columnOrder: ["column-1", "column-2", "column-3"],
-}
-*/
