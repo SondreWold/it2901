8053e5fe4e4e4e3fc5080d760f68aa4684df3ff1
Miriam Lillebo <miriamsl@stud.ntnu.no>
2019-03-01 10:16:07 +0100

    Cleaned up code from codereview, half time

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,15 @@ import React, { Component } from "react";
 import "./App.css";
 import NavigationContainer from "./containers/NavigationContainer";
 import DisplayContainer from './containers/DisplayContainer';
-//De der sticky greiene funker ikke hos meg
-//import StickyContainer from 'react-sticky-header';
-//import 'react-sticky-header/styles.css';
 
 class App extends Component {
-  state = { text: "" };
-
+  
   render() {
     return (
-
       <div className="App">
         <NavigationContainer/>
         <DisplayContainer/>
-
       </div>
-
     );
   }
 }
