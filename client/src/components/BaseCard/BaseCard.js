1776add3ae73e4cb44fd4248286cac3aa81ce460
sondre <sondrewold1@gmail.com>
2019-03-05 11:01:56 +0100

    BUGFIX: If there are noe rows for absencec_children, insert 0 values for given date on all bases #131

--- client/src/components/BaseCard/BaseCard.js
+++ client/src/components/BaseCard/BaseCard.js
@@ -8,55 +8,60 @@ import EmployeesAtBase from "./EmployeesAtBase";
 import EmployeesNeeded from "./EmployeesNeeded";
 
 // the proposed number of employees / children
-const FACTOR =  0.160;
+const FACTOR = 0.16;
 
 class BaseCard extends Component {
-
   calculateEmployeesAtBase = () => {
     var employees = [];
     let totalEmployeesAtBase = 0;
     let absentEmployeesAtBase = 0;
     this.props.employees.map(employee => {
-      if(employee.base_id === this.props.base.id) {
+      if (employee.base_id === this.props.base.id) {
         totalEmployeesAtBase++;
-        if(this.props.absentEmployees.length > 0) {
-        this.props.absentEmployees.map(absent => {
-          if((absent.employee_id === employee.id) &&
-            (moment(this.props.date).format("YYYY-MM-DD") ===
-            moment(absent.date).format("YYYY-MM-DD"))) {
-              console.log(((absent.employee_id === employee.id) &&
-                (moment(this.props.date).format("YYYY-MM-DD") ===
-                moment(absent.date).format("YYYY-MM-DD"))));
+        if (this.props.absentEmployees.length > 0) {
+          this.props.absentEmployees.map(absent => {
+            if (
+              absent.employee_id === employee.id &&
+              moment(this.props.date).format("YYYY-MM-DD") ===
+                moment(absent.date).format("YYYY-MM-DD")
+            ) {
+              console.log(
+                absent.employee_id === employee.id &&
+                  moment(this.props.date).format("YYYY-MM-DD") ===
+                    moment(absent.date).format("YYYY-MM-DD")
+              );
               absentEmployeesAtBase++;
-          }});
+            }
+          });
         }
       }
     });
     employees.push(totalEmployeesAtBase, absentEmployeesAtBase);
     return employees;
-  }
+  };
 
-  colorRendering = (value) => {
-		let color = "#FFFB94";
-		if (value >= 0) {
-			color = "#B2F1AF";
-		} else if (value < -1) {
-			color = "#FF8989";
-		}
-		return color;
-
-	}
+  colorRendering = value => {
+    let color = "#FFFB94";
+    if (value >= 0) {
+      color = "#B2F1AF";
+    } else if (value < -1) {
+      color = "#FF8989";
+    }
+    return color;
+  };
 
   render() {
     let employeesAtBase = this.calculateEmployeesAtBase();
-  	// calc of needed employees
-  	const employeesPresent = this.props.dragEmployees.length;
-  	const childrenPresent = this.props.absence.total_children - this.props.absence.children;
-  	const neededEmployees = employeesPresent - Math.round(childrenPresent * FACTOR);
-  	const color = this.colorRendering(neededEmployees);
+    // calc of needed employees
+    const employeesPresent = this.props.dragEmployees.length;
+    const childrenPresent =
+      this.props.absence.total_children - this.props.absence.children;
+    const neededEmployees =
+      employeesPresent - Math.round(childrenPresent * FACTOR);
+    const color = this.colorRendering(neededEmployees);
 
     return (
-      <div style={Container, {backgroundColor: color}}>
+      <div style={(Container, { backgroundColor: color })}>
         <BaseCardHeader baseName={this.props.base.name} />
         <ChildrenPresent
           base={this.props.absence.base_id}
@@ -71,9 +76,7 @@ class BaseCard extends Component {
           update={this.props.update}
         />
         <EmployeesAtBase baseEmployees={employeesAtBase} />
-        <EmployeesNeeded
-	      	neededEmployees={neededEmployees}
-        />
+        <EmployeesNeeded neededEmployees={neededEmployees} />
         <BaseCardList
           key={this.props.dragBase.id}
           dragBase={this.props.dragBase}
@@ -90,5 +93,5 @@ const Container = {
   border: "1px solid black",
   borderRadius: "5px",
   margin: "10px",
-  minHeight: "300px",
+  minHeight: "300px"
 };
