68bb8898a0d83065cabeb8913f8cb0e4828cce4f
haavarhu <havard.hunshamar@ntnu.no>
2019-03-18 14:59:19 +0100

    fixed drag and drop ui bug #63

--- client/src/reducers/workingEmployeesReducer.js
+++ client/src/reducers/workingEmployeesReducer.js
@@ -1,5 +1,5 @@
 import { UPDATE_WORKING_EMPLOYEES } from "../actions/contentActions/contentEmployeeActions";
-
+import { UPDATE_SINGLE_WORKING_EMPLOYEE } from "../actions/movedEmployeeAction";
 const initialState = {
   data: []
 };
@@ -12,6 +12,13 @@ export default function movedReducer(state = initialState, action) {
         data: action.payload.data
       };
 
+    case UPDATE_SINGLE_WORKING_EMPLOYEE:
+      return {
+        ...state,
+        data: state.data.map(emp =>
+          emp.employee_id === action.payload.employee_id ? action.payload : emp
+        )
+      };
     default:
       return state;
   }
