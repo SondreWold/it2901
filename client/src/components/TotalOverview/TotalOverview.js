77be584d9f290f30466fade4169539154f1bba3b
haavarhu <havard.hunshamar@ntnu.no>
2019-03-18 13:44:58 +0100

    fixed employeecounter

--- client/src/components/TotalOverview/TotalOverview.js
+++ client/src/components/TotalOverview/TotalOverview.js
@@ -5,22 +5,27 @@ import moment from "moment";
 import "./TotalOverview.css";
 
 class TotalOverview extends Component {
-
   getTotalOverview = () => {
-  var result = [];
-  let totalChildren = 0;
-  let totalAbsentChildren = 0;
-  let totalEmployees = this.props.employees.length;
-  if (this.props.children.length > 0) {
-    this.props.children.map(child => {
-      totalChildren += child.total_children;
-      totalAbsentChildren += child.children;
-    });
-  }
-  let totalAbsentEmployees = totalEmployees - this.props.moved_employees.length ;
-  result.push(totalChildren, totalAbsentChildren, totalAbsentEmployees, totalEmployees);
-  return result;
-  }
+    var result = [];
+    let totalChildren = 0;
+    let totalAbsentChildren = 0;
+    let totalEmployees = this.props.employees.length;
+    if (this.props.children.length > 0) {
+      this.props.children.map(child => {
+        totalChildren += child.total_children;
+        totalAbsentChildren += child.children;
+      });
+    }
+    let totalAbsentEmployees =
+      totalEmployees - this.props.working_employees.length;
+    result.push(
+      totalChildren,
+      totalAbsentChildren,
+      totalAbsentEmployees,
+      totalEmployees
+    );
+    return result;
+  };
 
   render() {
     let overviewList = this.getTotalOverview();
@@ -32,10 +37,10 @@ class TotalOverview extends Component {
           minDate={this.props.minDate}
         />
         <TotalPresence
-        totalChildren={overviewList[0]}
-        totalAbsentChildren = {overviewList[1]}
-        totalAbsentEmployees = {overviewList[2]}
-        totalEmployees = {overviewList[3]}
+          totalChildren={overviewList[0]}
+          totalAbsentChildren={overviewList[1]}
+          totalAbsentEmployees={overviewList[2]}
+          totalEmployees={overviewList[3]}
         />
       </div>
     );
