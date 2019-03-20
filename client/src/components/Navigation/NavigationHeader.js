2ec3c11269b1c480b15ab4cd7da40d1edf4799ab
haavarhu <havard.hunshamar@ntnu.no>
2019-03-20 16:27:45 +0100

    endret farge i navbar

--- client/src/components/Navigation/NavigationHeader.js
+++ client/src/components/Navigation/NavigationHeader.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import "./Navigation.css";
-import {NavLink} from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 class NavigationHeader extends Component {
   render() {
     return (
-      <div className="NavigationHeader"> 
+      <div className="NavigationHeader">
         <NavLink className="NavbarLink" exact to="/">
           {this.props.pageHeader}
-        </NavLink> 
+        </NavLink>
       </div>
-    ) 
+    );
   }
 }
 
