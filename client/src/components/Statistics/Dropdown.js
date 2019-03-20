3b8f776b5cd3823b37ee3d7e017e28d44fc5ebb3
ingridlarssen <ingrid_al_96@hotmail.com>
2019-03-20 21:06:39 +0100

    graph with dummy data #256

---
+++ client/src/components/Statistics/Dropdown.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Dropdown extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Dette skal bli en dropdown for å velge hvilken graf du vil se</p>
+      </div>
+    );
+  }
+}
+
+export default Dropdown;
