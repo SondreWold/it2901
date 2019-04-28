117324d74e3ac58da8714c2cbb526d3d93891497
ulrikah <ulrikhalmoy@gmail.com>
2019-04-28 20:47:07 +0200

    add dnd test #391

--- client/cypress/integration/homepage/dnd.spec.js
+++ client/cypress/integration/homepage/dnd.spec.js
@@ -11,20 +11,25 @@ Expected behaviour: the "drag container" should have one employee less.
 The "drop container" should have one more
 
 */
-describe("Drag and drop employees", () => {
-
-  it("drag an employee from the first base to the second", () => {
-  	cy.get('[data-react-beautiful-dnd-draggable]')
-  		.first()
-  		.drag()
-  		.wait(500)
-	  cy.get('[data-react-beautiful-dnd-droppable]')
-	  	.last()
-	  	.drop();
-  });
+describe("Drag and drop employees", function(){
+	const draggable = '[data-react-beautiful-dnd-draggable]';
+	const droppable = '[data-react-beautiful-dnd-droppable]';
 
   beforeEach(function(){
       cy.viewport(1400, 1000)
       cy.visit('localhost:3000/')
+      cy.get(draggable).first().invoke('text').as('emp')
   })
+
+  it("drag an employee from the first base to the last", function(){		
+  	cy.get(draggable)
+  		.first()
+ 			.drag()
+
+	  cy.get(droppable)
+	  	.last()
+	  	.drop()
+	  	.wait(500)
+	  	.contains(this.emp)
+  });
 });
